91f63284e7371d478c800eaf2c95ee638f2176fc
TheCodeGhninux <ilesanmibry@gmail.com>
2025-11-16 23:29:00 +0100

    feat: init user module - cleaned up redundant modules and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth-service/auth.module';
-import { UserModule } from './user-service/user.module';
-import { AcademicModule } from './academic-service/academic.module';
-import { AttendanceModule } from './attendance-service/attendance.module';
-import { FeeModule } from './fee-service/fee.module';
-import { NotificationModule } from './notification-service/notification.module';
-import { AnalyticsModule } from './analytics-service/analytics.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [
-    AuthModule,
-    UserModule,
-    AcademicModule,
-    AttendanceModule,
-    FeeModule,
-    NotificationModule,
-    AnalyticsModule,
-  ],
+  imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
