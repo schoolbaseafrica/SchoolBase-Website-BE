c30f9bfd14a918dec45822aae03616ddbf5b1f7a
Praiz001 <oshilimpraisek@gmail.com>
2025-11-20 14:08:22 +0100

    fix(test): resolve authservice failing test

--- src/modules/auth/auth.service.spec.ts
+++ src/modules/auth/auth.service.spec.ts
@@ -1,13 +1,71 @@
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Logger } from 'winston';
+
+import { EmailService } from '../email/email.service';
+import { SessionService } from '../session/session.service';
+import { UserService } from '../user/user.service';
 
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
+  const mockUserService = {
+    findByEmail: jest.fn(),
+    create: jest.fn(),
+    findOne: jest.fn(),
+    updateUser: jest.fn(),
+    findByResetToken: jest.fn(),
+  };
+
+  const mockLogger = {
+    child: jest.fn().mockReturnThis(),
+    error: jest.fn(),
+    warn: jest.fn(),
+    info: jest.fn(),
+    debug: jest.fn(),
+  } as unknown as Logger;
+
+  const mockJwtService = {
+    signAsync: jest.fn(),
+    verifyAsync: jest.fn(),
+  };
+
+  const mockEmailService = {
+    sendMail: jest.fn(),
+  };
+
+  const mockSessionService = {
+    createSession: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: UserService,
+          useValue: mockUserService,
+        },
+        {
+          provide: WINSTON_MODULE_PROVIDER,
+          useValue: mockLogger,
+        },
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+        {
+          provide: EmailService,
+          useValue: mockEmailService,
+        },
+        {
+          provide: SessionService,
+          useValue: mockSessionService,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
