cdac50c57d79afa7ecc1ebf470cedf45cb4638f1
ObiFaith <obifth@gmail.com>
2025-11-20 23:10:28 +0100

    feat: add school model and term resources

---
+++ src/modules/term/term.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+
+import { TermController } from './term.controller';
+import { TermService } from './term.service';
+
+@Module({
+  controllers: [TermController],
+  providers: [TermService],
+})
+export class TermModule {}
