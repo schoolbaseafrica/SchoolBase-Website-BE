e4ef166d83b0fb24d5f856bfde2c04ecaf95ddbc
iamarvy <okeseyi5@gmail.com>
2025-11-28 21:36:09 +0100

    chore(class-student): add class student entity and model action to barren export

--- src/modules/class/entities/index.ts
+++ src/modules/class/entities/index.ts
@@ -1,3 +1,4 @@
 export * from './class-subject.entity';
 export * from './class-teacher.entity';
 export * from './class.entity';
+export * from './class-student.entity';
--- src/modules/class/model-actions/index.ts
+++ src/modules/class/model-actions/index.ts
@@ -1,3 +1,4 @@
 export * from './class-teacher.action';
 export * from './class.actions';
 export * from './clsas-subject.action';
+export * from './class-student.action';
