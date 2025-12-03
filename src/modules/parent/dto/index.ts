c71370c9b85fac78176642c06521ebaa0e8182e0
Biglens <adedejibioku@gmail.com>
2025-12-03 23:18:43 +0100

    chore: barel export parent dto

--- src/modules/parent/dto/index.ts
+++ src/modules/parent/dto/index.ts
@@ -4,3 +4,4 @@ export * from './parent-response.dto';
 export * from './list-parents.dto';
 export * from './link-students.dto';
 export * from './parent-student-link-response.dto';
+export * from './student-basic.dto';
